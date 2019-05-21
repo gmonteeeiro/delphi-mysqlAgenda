b1e9ce4132e3cec084b3082e919adffc7b01db9e
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-05-21 20:43:13 -0300

    Remove fClientes

--- Agenda/Agenda.dpr
+++ Agenda/Agenda.dpr
@@ -3,7 +3,6 @@ program Agenda;
 uses
   Vcl.Forms,
   fMain in 'fMain.pas' {FrmMain},
-  fClientes in 'fClientes.pas' {FrmClientes},
   fDataModule in 'fDataModule.pas' {DataModule1: TDataModule};
 
 {$R *.res}
@@ -12,7 +11,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFrmMain, FrmMain);
-  Application.CreateForm(TFrmClientes, FrmClientes);
   Application.CreateForm(TDataModule1, DataModule1);
   Application.Run;
 end.
